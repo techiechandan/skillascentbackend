dac2da5b98a1b35619d1ede99e0818ba2359b622
techiechandan <techiechandan8@gmail.com>
2023-06-17 11:48:19 +0530

    new commit, added samesite option to the coolies option

--- controllers/appController.js
+++ controllers/appController.js
@@ -7,13 +7,15 @@ const SiteModel = require('../model/SiteModel');
 const cookieOption1 = {
     maxAge:Date.now()+60*60*1000, 
     httpOnly: true,
-    domain: "skillascentbackend.onrender.com"
+    domain: "skillascentbackend.onrender.com",
+    sameSite: 'lax'
 }
 
 const cookieOption2 = {
     maxAge:Date.now()+30*24*60*60*1000, 
     httpOnly: true,
-    domain: "skillascentbackend.onrender.com"
+    domain: "skillascentbackend.onrender.com",
+    sameSite: 'lax'
 }
 
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -14,13 +14,15 @@ const userAuth = require('../auth/userAuth')
 const cookieOption1 = {
     maxAge:Date.now()+60*60*1000, 
     httpOnly: true,
-    domain: "skillascentbackend.onrender.com"
+    domain: "skillascentbackend.onrender.com",
+    sameSite: 'lax'
 }
 
 const cookieOption2 = {
     maxAge:Date.now()+30*24*60*60*1000, 
     httpOnly: true,
-    domain: "skillascentbackend.onrender.com"
+    domain: "skillascentbackend.onrender.com",
+    sameSite: 'lax'
 }
 
 const getRegister = async (req, res) => {
